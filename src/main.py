cc2934631161b7ce1f86d554cb8542a088e8fff5
victor <zhouhao3290@gmail.com>
2024-11-01 09:59:35 +0800

    add --head before ctld

--- src/main.py
+++ src/main.py
@@ -27,12 +27,16 @@ def main():
 
     ## 初始化
     init()
+    cur_path = os.getcwd()
     try:
         mininet_service = MininetService(MININET_SHELL_COMMAND, TEST_FRAME_PATH, 'mininet.log').start()  ## 启动mininet虚拟化craned
         if mininet_service is None:
             reset()
             exit(1)
 
+        os.chdir(TEST_FRAME_PATH)
+        run_shell_command(MININET_INIT_SHELL_COMMAND)
+        os.chdir(cur_path)
         ctld_service = CraneCtldService(CTLD_SHELL_COMMAND, 'ctld.log').start()   ## 启动ctld服务
         if ctld_service is None:
             mininet_service.stop()
